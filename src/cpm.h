962d4517e980164a9440964e29cd4d38fac496d6
jwells167 <jwells167@bloomberg.net>
2026-06-29 12:44:46 +0100

    Add types

--- src/cpm.h
+++ src/cpm.h
@@ -1,6 +1,13 @@
 #ifndef CPM_H
 #define CPM_H
 
-
+typedef struct {
+    int earliest_start;
+    int earliest_finish;
+    int latest_start;
+    int latest_finish;
+    int free_float;
+    int total_float;
+} CPMResult;
 
 #endif /* CPM_H */
